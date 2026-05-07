db4c37c4859c214fe789f2e2daa2844e4997d34c
AkhiSharmin <akhisharmin100@gmail.com>
2026-05-07 21:56:42 +0600

    navber done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 import "./App.css";
-import navImg from "./assets/logo.png";
-import dollarImg from "./assets/dollar.png";
+
+import AvailablePlayers from "./components/AvailablePlayers/AvailablePlayers";
+import Navbar from "./components/Navbar/Navbar";
+import SelectedPlayers from "./components/SelectedPlayers/SelectedPlayers";
 
 function App() {
   return (
     <>
-      <div className="navbar bg-base-100 shadow-sm max-w-7xl mx-auto">
-        <div className="flex-1">
-          <a className=" text-xl">
-            <img className="w-[60px] h-[60px]" src={navImg} alt="" />
-          </a>
-        </div>
-        <div className="flex items-center gap-1">
-          <span>600000000</span>
-          <span>Coin</span>
-          <img src={dollarImg} alt="" />
-        </div>
-      </div>
+      <Navbar></Navbar>
+
+      <AvailablePlayers></AvailablePlayers>
+
+      <SelectedPlayers></SelectedPlayers>
     </>
   );
 }
---
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AvailablePlayers = () => {
+  return (
+    <div>
+      <h2>Available Players</h2>
+    </div>
+  );
+};
+
+export default AvailablePlayers;
