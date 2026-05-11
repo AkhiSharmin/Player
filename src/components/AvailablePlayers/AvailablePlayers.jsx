6273cd97cb77761c491da93bb29bc8ceeb973843
AkhiSharmin <akhisharmin100@gmail.com>
2026-05-11 22:14:55 +0600

    update file

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -6,42 +6,43 @@ const AvailablePlayers = ({ playersPromise }) => {
   const playersData = use(playersPromise);
   console.log(playersData);
   return (
-    <div className="max-w-7xl mx-auto ">
+    <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-6 ">
       {playersData.map((player) => (
-        <div className="card bg-base-100 w-96 shadow-sm p-4 ">
+        <div className="card bg-base-100 shadow-sm p-8 border-b-2">
           <figure>
             <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+              className="w-full h-[300px] object-cover"
+              src={player.playerImg}
               alt="Shoes"
             />
           </figure>
           <div className="mt-4">
             <div className="flex">
               <img src={userImg} alt="" />
-              <h2 className="card-title ml-2">Virat koheli</h2>
+              <h2 className="card-title ml-2">{player.playerName}</h2>
             </div>
 
             <div className="flex justify-between mt-4">
               <div className="flex gap-2 items-center ">
                 <img className="w-5 h-4" src={flagImg} alt="" />
-                <span className="">India</span>
+                <span className="">{player.playerCountry}</span>
               </div>
-              <button className="btn">All Rounder</button>
+              <button className="btn">{player.playingRole}</button>
             </div>
             <div className="divider divider-start"></div>
 
             <div className="flex justify-between items-center font-bold mt-4">
               <span>Rating</span>
-              <span>5</span>
+              <span>{player.rating}</span>
             </div>
 
             <div className="flex justify-between ">
-              <span className="font-bold">Left hand bat</span>
-              <span className="text-slate-400">Right hand bowl</span>
+              <span className="font-bold">{player.battingStyle}</span>
+              <span className="text-slate-400">{player.bowlingStyle}</span>
             </div>
 
             <div className="card-actions mt-3 flex justify-between items-center">
-              <p className="font-bold">Price: $150000</p>
+              <p className="font-bold">Price: ${player.price}</p>
               <button className="btn">Choose Player</button>
             </div>
           </div>
