e03f5c04efc10eed8c245cd1db6aeb1198083df9
AkhiSharmin <akhisharmin100@gmail.com>
2026-06-15 22:54:23 +0600

    some update

---
+++ src/components/SelectedCard/SelectedCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SelectedCard = () => {
+  return (
+    <div>
+      <h2>SelectedCard</h2>
+    </div>
+  );
+};
+
+export default SelectedCard;
--- src/components/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -4,7 +4,22 @@ const SelectedPlayers = ({ purchasedPlayers }) => {
   console.log(purchasedPlayers);
   return (
     <div className="max-w-7xl mx-auto">
-      <h2>Selected Players</h2>
+      <div className="border-2 border-slate-500 rounded-xl p-3 flex justify-between items-center">
+        <div className="flex items-center">
+          <img
+            className="h-12 w-12 rounded-xl"
+            src="https://i.ibb.co.com/c54zZ7J/1696748634550-Shakib-Al-Hasan.jpg"
+            alt=""
+          />
+          <div className="ml-2">
+            <h2>Sakib Al Hasan</h2>
+            <p className="text-xs">Left-hand-bat</p>
+          </div>
+        </div>
+        <div>
+          <img src="https://i.ibb.co.com/7N2Rk7gC/dlt.png" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
